cfd416ebd893ee0edf269d4af55b502679f40d4a
maja rotovnik <maja.rotovnik001@gmail.com>
2024-03-10 10:47:11 +0100

    first commit

---
+++ src/main/java/com/example/eventTicket/Service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.eventTicket.Service;
+
+import com.example.eventTicket.Entitiy.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User saveUser(User user);
+    List<User> fetchAllUsers();
+
+    User getUserById(String id);
+
+    User getUserByName(String firstName);
+
+    User updateUser(String id, User user);
+    String deleteUser(String id);
+}
